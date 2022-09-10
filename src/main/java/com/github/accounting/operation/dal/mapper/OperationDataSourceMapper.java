1aea8bd13938738f95d349eef3c0b1cffed55d86
idevtier <idevtier@gmail.com>
2022-09-10 23:03:57 +0300

    Refactoring datasource mapper

--- src/main/java/com/github/accounting/operation/dal/mapper/OperationDataSourceMapper.java
+++ src/main/java/com/github/accounting/operation/dal/mapper/OperationDataSourceMapper.java
@@ -1,33 +1,30 @@
 package com.github.accounting.operation.dal.mapper;
 
-import com.github.accounting.operation.dal.datasource.JoinedOperationDataSource;
+import com.github.accounting.operation.dal.datasource.OperationDSReadResponseModel;
 import io.r2dbc.spi.Row;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 import java.util.function.BiFunction;
 
 @Component
-public class OperationDataSourceMapper implements BiFunction<Row, Object, JoinedOperationDataSource> {
+public class OperationDataSourceMapper implements BiFunction<Row, Object, OperationDSReadResponseModel> {
 
     @Override
     @SneakyThrows
-    public JoinedOperationDataSource apply(Row row, Object o) {
-        var operation_id = Objects.requireNonNull(row.get("operation_id", Long.class));
-        var operation_sum = Objects.requireNonNull(row.get("operation_sum", Double.class));
-        var dateAsString = Objects.requireNonNull(row.get("operation_created_at")).toString();
-        var createdAt = LocalDateTime.parse(dateAsString);
-
-        return JoinedOperationDataSource.builder()
-                .operationId(operation_id)
-                .sum(operation_sum)
+    public OperationDSReadResponseModel apply(Row row, Object o) {
+        return OperationDSReadResponseModel.builder()
+                .operationId(row.get("operation_id", Long.class))
+                .employeeId(row.get("employee_id", Long.class))
+                .toEmployeeId(row.get("to_employee_id", Long.class))
+                .employeeAuthority(row.get("employee_authority", String.class))
+                .sum(row.get("operation_sum", Double.class))
                 .description(row.get("description", String.class))
                 .categoryName(row.get("category_name", String.class))
                 .fromAccountName(row.get("from_account_name", String.class))
                 .toAccountName(row.get("to_account_name", String.class))
-                .createdAt(createdAt)
+                .createdAt(row.get("operation_created_at", LocalDateTime.class))
                 .build();
     }
 }
