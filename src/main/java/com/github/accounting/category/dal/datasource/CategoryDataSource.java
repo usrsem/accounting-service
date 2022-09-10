eee84c5154e35f35cf4cd737b83d346656a71aaf
idevtier <idevtier@gmail.com>
2022-09-10 23:05:53 +0300

    Adding category datasource

---
+++ src/main/java/com/github/accounting/category/dal/datasource/CategoryDataSource.java
@@ -0,0 +1,20 @@
+package com.github.accounting.category.dal.datasource;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+
+@Table("category")
+@AllArgsConstructor
+@Getter
+public class CategoryDataSource {
+    @Id
+    @Column("id")
+    private int id;
+
+    @Column("name")
+    private String name;
+}
