309f1d74e4259ac4eaf5d55400b78d7c405d6ae2
idevtier <idevtier@gmail.com>
2022-09-10 23:06:20 +0300

    Adding authority field to employee table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE employee
 (
     id        BIGINT auto_increment NOT NULL,
     full_name VARCHAR(100)          NOT NULL,
+    authority VARCHAR(20)           NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -46,17 +47,39 @@ CREATE TABLE operation
 );
 
 INSERT INTO employee
-VALUES (1, 'Andrey');
+VALUES (default, 'Andrey', 'CHIEF');
 INSERT INTO employee
-VALUES (2, 'Tagir');
+VALUES (default, 'Tagir', 'WORKER');
 
 INSERT INTO account
-VALUES (1, 'Andrey', 5000.0, FALSE, 1);
+VALUES (default, 'Andrey', 5000.0, FALSE, 1);
 INSERT INTO account
-VALUES (2, 'Tagir', 10000.0, FALSE, 2);
+VALUES (default, 'Tagir', 10000.0, FALSE, 2);
+INSERT INTO account
+VALUES (default, 'Tinkoff', 0.0, TRUE, NULL);
 
 INSERT INTO category
-VALUES (1, 'transfer');
+VALUES (default, 'transfer');
 
 INSERT INTO operation
-VALUES (default, 'Some description', 500.0, 2, 1, 1, 1, default);
+VALUES (default, 'Some description', 500.0, 1, 2, 1, 1, default);
+INSERT INTO operation
+VALUES (default, 'Some description', 500.0, 3, 2, 1, 2, default);
+INSERT INTO operation
+VALUES (default, 'Some description', 500.0, 1, 2, 1, 1, default);
+
+SELECT o.id           as operation_id,
+       o.employee_id  as employee_id,
+       ta.employee_id as to_employee_id,
+       o.sum          as operation_sum,
+       o.created_at   as operation_created_at,
+       c.name         as category_name,
+       fa.name        as from_account_name,
+       ta.name        as to_account_name,
+       e.authority    as employee_authority,
+       description
+from operation as o
+         LEFT JOIN category c ON o.category_id = c.id
+         LEFT JOIN account fa on o.from_account_id = fa.id
+         LEFT JOIN account ta on o.to_account_id = ta.id
+         LEFT JOIN employee e on o.employee_id = e.id;
