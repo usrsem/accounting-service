b778e66710573b00f5edb19d67bb4254c7cbe3b5
idevtier <idevtier@gmail.com>
2022-09-26 18:51:52 +0300

    Adding SQL script for clearing db

---
+++ src/main/resources/clear.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS authority_relation;
+DROP TABLE IF EXISTS category_relation;
+DROP TABLE IF EXISTS operation;
+DROP TABLE IF EXISTS account;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS employee;
+
