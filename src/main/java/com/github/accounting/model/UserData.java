383ce7f8081be6966f3943cf4240d779caf82dd4
usrsem <andrey7menov@gmail.com>
2022-10-06 12:10:00 +0300

    Adding method for getting role from authorities

--- src/main/java/com/github/accounting/model/UserData.java
+++ src/main/java/com/github/accounting/model/UserData.java
@@ -3,4 +3,13 @@ package com.github.accounting.model;
 import java.util.Collection;
 
 public record UserData(long id, String name, Collection<String> authorities) {
+    public EmployeeAuthority getRole() {
+        var role = authorities.stream()
+                .filter(authority -> authority.startsWith("ROLE_"))
+                .findAny();
+        if (role.isEmpty()) {
+            throw new RuntimeException("User have not any role");
+        }
+        return EmployeeAuthority.valueOf(role.get());
+    }
 }
