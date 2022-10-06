f4b103ba76ed6999cebc7d789ee7196a462d141f
usrsem <andrey7menov@gmail.com>
2022-10-06 12:02:57 +0300

    Deleting account package from root package

--- src/main/java/com/github/accounting/account/dal/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.accounting.account.dal.repository;
-
-import com.github.accounting.account.dal.datasource.AccountDataSource;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface AccountRepository extends ReactiveCrudRepository<AccountDataSource, Long> {
-}
--- src/main/java/com/github/accounting/account/model/boundary/input/AccountReadRequestModel.java
+++
@@ -1,8 +0,0 @@
-package com.github.accounting.account.model.boundary.input;
-
-import com.github.accounting.model.UserData;
-
-public record AccountReadRequestModel(
-        UserData userData
-) {
-}
